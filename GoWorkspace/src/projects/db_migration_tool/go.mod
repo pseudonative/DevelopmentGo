7a4d853fc08f5d558a69838913166775660ec5c7
Jeremy Coando <killerpit02@yahoo.com>
2023-11-30 18:49:26 -0700

    LL

--- GoWorkspace/src/projects/db_migration_tool/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/pseudonative/DevelopmentGo/GoWorkspace/src/projects/db_migration_tool
-
-go 1.21.4
-
-require github.com/lib/pq v1.10.9
--- GoWorkspace/src/projects/db_migration_tool/migrations/20231130092856_test_migration.sql
+++
@@ -1,5 +0,0 @@
--- UP
-CREATE TABLE IF NOT EXISTS example_table (id SERIAL PRIMARY KEY, name VARCHAR(255));
-
--- DOWN
-DROP TABLE IF EXISTS example_table;
