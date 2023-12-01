3acf418d94bf8899ba3c0d4be563b3e93f2a40fe
Jeremy Coando <killerpit02@yahoo.com>
2023-11-30 19:15:33 -0700

    prod

---
+++ GoWorkspace/src/projects/real_time_data_analytics_platform/terraform/prod/providers.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  alias  = "produseast1"
+  region = "us-east-1"
+
+  assume_role {
+    role_arn     = "arn:aws:iam::778339273960:role/Test2FullAccess"
+    session_name = "Terraform-DevUpdate"
+  }
+}
+
+provider "aws" {
+  alias  = "devuseast1"
+  region = "us-east-1"
+
+  assume_role {
+    role_arn     = "arn:aws:iam::520291287938:role/ProdFullAccess"
+    session_name = "Terraform-ProdUpdate"
+  }
+}
