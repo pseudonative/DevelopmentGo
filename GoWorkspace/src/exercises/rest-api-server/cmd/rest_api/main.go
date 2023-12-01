af95997d8c6e952eea114b5d40aa946882e34b21
Jeremy Coando <killerpit02@yahoo.com>
2023-12-01 10:51:49 -0700

    screwed up

--- GoWorkspace/src/exercises/rest-api-server/cmd/rest_api/main.go
+++ GoWorkspace/src/exercises/rest-api-server/cmd/rest_api/main.go
@@ -8,16 +8,8 @@ import (
 
 func main() {
 	http.HandleFunc("/items", handleItems)
-	http.HandleFunc("/items", handleItem)
+	http.HandleFunc("/item", handleItem)
 
 	fmt.Println("Server is running on port 8080...")
 	log.Fatal(http.ListenAndServe(":8080", nil))
-}
-
-func handleItems(w http.ResponseWriter, r *http.Request) {
-
-}
-
-func handleItem(w http.ResponseWriter, r *http.Request) {
-
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/pseudonative/DevelopmentGo
 
 go 1.21.4
+
+require github.com/pseudonative/DevelopmentGo/GoWorkspace/src/exercises/rest-api-server v0.0.0-20231201164746-6931e6d8e06c
