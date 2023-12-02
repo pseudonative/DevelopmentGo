05d7e4fcdae79c14fb33a157dfb8ae1610b5099e
Jeremy Coando <killerpit02@yahoo.com>
2023-12-02 16:46:16 -0700

    generic from alias for speed

--- GoWorkspace/src/exercises/backend_webserver/go.mod
+++ GoWorkspace/src/exercises/backend_webserver/go.mod
@@ -1,3 +1,5 @@
 module github.com/pseudonative/backend_webserver
 
 go 1.21.4
+
+require github.com/joho/godotenv v1.5.1 // indirect
--- GoWorkspace/src/exercises/backend_webserver/main.go
+++ GoWorkspace/src/exercises/backend_webserver/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
 	fmt.Println("Hello World")
+
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("Port is not found in the environment")
+	}
+	fmt.Println("port:", portString)
 }
