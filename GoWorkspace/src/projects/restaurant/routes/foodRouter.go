38050927fd576335a9f5ee0f917da0bd8b59a09f
Beau Coando <killerpit02@yahoo.com>
2023-12-15 12:09:58 -0700

    routes main controllers

--- GoWorkspace/src/projects/restaurant/routes/foodRouter.go
+++ GoWorkspace/src/projects/restaurant/routes/foodRouter.go
@@ -1,1 +1,13 @@
 package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	controller "github.com/pseudonative/restaurant/controllers"
+)
+
+func FoodRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.GET("/foods", controller.GetFoods())
+	incomingRoutes.GET("/foods/:food_id", controller.GetFood())
+	incomingRoutes.POST("foods", controller.CreateFood())
+	incomingRoutes.PATCH("foods/:food_id", controller.UpdateFood())
+}
