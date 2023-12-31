e8394943fbab403f7374466949171cb45c71a841
Jeremy Coando <killerpit02@yahoo.com>
2023-12-30 18:58:59 -0700

    finally solved the TestResolver testing with URL

--- GoWorkspace/src/projects/proglog/internal/loadbalance/resolver.go
+++ GoWorkspace/src/projects/proglog/internal/loadbalance/resolver.go
@@ -13,87 +13,6 @@ import (
 	"google.golang.org/grpc/serviceconfig"
 )
 
-// type Resolver struct {
-// 	mu            sync.Mutex
-// 	clientConn    resolver.ClientConn
-// 	resolverConn  *grpc.ClientConn
-// 	serviceConfig *serviceconfig.ParseResult
-// 	logger        *zap.Logger
-// }
-
-// var _ resolver.Builder = (*Resolver)(nil)
-
-// func (r *Resolver) Build(target resolver.Target, cc resolver.ClientConn, opts resolver.BuildOptions) (resolver.Resolver, error) {
-// 	r.logger = zap.L().Named("resolver")
-// 	r.clientConn = cc
-// 	var dialOpts []grpc.DialOption
-// 	if opts.DialCreds != nil {
-// 		dialOpts = append(
-// 			dialOpts,
-// 			grpc.WithTransportCredentials(opts.DialCreds),
-// 		)
-// 	}
-// 	r.serviceConfig = r.clientConn.ParseServiceConfig(
-// 		fmt.Sprintf(`{"loadBalancingConfig":[{"%s":{}}]}`, Name),
-// 	)
-// 	var err error
-// 	r.resolverConn, err = grpc.Dial(target.Endpoint, dialOpts...)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	r.ResolveNow(resolver.ResolveNowOptions{})
-// 	return r, nil
-// }
-
-// const Name = "proglog"
-
-// func (r *Resolver) Scheme() string {
-// 	return Name
-// }
-
-// func init() {
-// 	resolver.Register(&Resolver{})
-// }
-
-// var _ resolver.Resolver = (*Resolver)(nil)
-
-// func (r *Resolver) ResolveNow(resolver.ResolveNowOptions) {
-// 	r.mu.Lock()
-// 	defer r.mu.Unlock()
-// 	client := api.NewLogClient(r.resolverConn)
-// 	ctx := context.Background()
-// 	res, err := client.GetServers(ctx, &api.GetServersRequest{})
-// 	if err != nil {
-// 		r.logger.Error(
-// 			"failed to resolve server",
-// 			zap.Error(err),
-// 		)
-// 		return
-// 	}
-// 	var addrs []resolver.Address
-// 	for _, server := range res.Servers {
-// 		addrs = append(addrs, resolver.Address{
-// 			Addr: server.RpcAddr,
-// 			Attributes: attributes.New(
-// 				"is_leader",
-// 				server.IsLeader,
-// 			),
-// 		})
-// 	}
-// 	r.clientConn.UpdateState(resolver.State{
-// 		Addresses:     addrs,
-// 		ServiceConfig: r.serviceConfig,
-// 	})
-// }
-
-//	func (r *Resolver) Close() {
-//		if err := r.resolverConn.Close(); err != nil {
-//			r.logger.Error(
-//				"failed to close conn",
-//				zap.Error(err),
-//			)
-//		}
-//	}
 type Resolver struct {
 	mu            sync.Mutex
 	clientConn    resolver.ClientConn
@@ -102,15 +21,9 @@ type Resolver struct {
 	logger        *zap.Logger
 }
 
-// END: resolver_type
-// START: resolver_builder
 var _ resolver.Builder = (*Resolver)(nil)
 
-func (r *Resolver) Build(
-	target resolver.Target,
-	cc resolver.ClientConn,
-	opts resolver.BuildOptions,
-) (resolver.Resolver, error) {
+func (r *Resolver) Build(target resolver.Target, cc resolver.ClientConn, opts resolver.BuildOptions) (resolver.Resolver, error) {
 	r.logger = zap.L().Named("resolver")
 	r.clientConn = cc
 	var dialOpts []grpc.DialOption
@@ -124,7 +37,7 @@ func (r *Resolver) Build(
 		fmt.Sprintf(`{"loadBalancingConfig":[{"%s":{}}]}`, Name),
 	)
 	var err error
-	r.resolverConn, err = grpc.Dial(target.Endpoint, dialOpts...)
+	r.resolverConn, err = grpc.Dial(target.Endpoint(), dialOpts...)
 	if err != nil {
 		return nil, err
 	}
@@ -142,16 +55,13 @@ func init() {
 	resolver.Register(&Resolver{})
 }
 
-// END: resolver_builder
-
-// START: resolver_resolver
 var _ resolver.Resolver = (*Resolver)(nil)
 
 func (r *Resolver) ResolveNow(resolver.ResolveNowOptions) {
 	r.mu.Lock()
 	defer r.mu.Unlock()
 	client := api.NewLogClient(r.resolverConn)
-	// get cluster and then set on cc attributes
+
 	ctx := context.Background()
 	res, err := client.GetServers(ctx, &api.GetServersRequest{})
 	if err != nil {
--- GoWorkspace/src/projects/proglog/internal/loadbalance/resolver_test.go
+++ GoWorkspace/src/projects/proglog/internal/loadbalance/resolver_test.go
@@ -1,7 +1,9 @@
 package loadbalance_test
 
 import (
+	"fmt"
 	"net"
+	"net/url"
 	"testing"
 
 	api "github.com/pseudonative/proglog/api/v1"
@@ -16,97 +18,6 @@ import (
 	"google.golang.org/grpc/serviceconfig"
 )
 
-// func TestResolver(t *testing.T) {
-// 	l, err := net.Listen("tcp", "127.0.0.1:0")
-// 	require.NoError(t, err)
-
-// 	tlsConfig, err := config.SetupTLSConfig(config.TLSConfig{
-// 		CertFile:      config.ServerCertFile,
-// 		KeyFile:       config.ServerKeyFile,
-// 		CAFile:        config.CAFile,
-// 		Server:        true,
-// 		ServerAddress: "127.0.0.1",
-// 	})
-// 	require.NoError(t, err)
-// 	serverCreds := credentials.NewTLS(tlsConfig)
-
-// 	srv, err := server.NewGRPCServer(&server.Config{
-// 		GetServerer: &getServers{},
-// 	}, grpc.Creds(serverCreds))
-// 	require.NoError(t, err)
-
-// 	go srv.Serve(l)
-
-// 	conn := &clientConn{}
-// 	tlsConfig, err = config.SetupTLSConfig(config.TLSConfig{
-// 		CertFile:      config.RootClientCertFile,
-// 		KeyFile:       config.RootClientKeyFile,
-// 		CAFile:        config.CAFile,
-// 		Server:        false,
-// 		ServerAddress: "127.0.0.1",
-// 	})
-// 	require.NoError(t, err)
-// 	clientCreds := credentials.NewTLS(tlsConfig)
-// 	opts := resolver.BuildOptions{
-// 		DialCreds: clientCreds,
-// 	}
-// 	r := &loadbalance.Resolver{}
-// 	_, err = r.Build(
-// 		resolver.Target{
-// 			Endpoint: l.Addr().String(),
-// 		},
-// 		conn,
-// 		opts,
-// 	)
-// 	require.NoError(t, err)
-// 	wantState := resolver.State{
-// 		Addresses: []resolver.Address{{
-// 			Addr:       "localhost:9001",
-// 			Attributes: attributes.New("is_leader", true),
-// 		}, {
-// 			Addr:       "localhost:9002",
-// 			Attributes: attributes.New("is_leader", false),
-// 		}},
-// 	}
-// 	require.Equal(t, wantState, conn.state)
-
-// 	conn.state.Addresses = nil
-// 	r.ResolveNow(resolver.ResolveNowOptions{})
-// 	require.Equal(t, wantState, conn.state)
-// }
-
-// type getServers struct{}
-
-// func (s *getServers) GetServers() ([]*api.Server, error) {
-// 	return []*api.Server{{
-// 		Id:       "leader",
-// 		RpcAddr:  "localhost:9001",
-// 		IsLeader: true,
-// 	}, {
-// 		Id:      "follower",
-// 		RpcAddr: "localhost:9002",
-// 	}}, nil
-// }
-
-// type clientConn struct {
-// 	resolver.ClientConn
-// 	state resolver.State
-// }
-
-// func (c *clientConn) UpdateState(state resolver.State) {
-// 	c.state = state
-
-// }
-
-// func (c *clientConn) ReportError(err error) {}
-
-// func (c *clientConn) NewAddress(addrs []resolver.Address) {}
-
-// func (c *clientConn) NewServiceConfig(config string) {}
-
-//	func (c *clientConn) ParseServiceConfig(config string) *serviceconfig.ParseResult {
-//		return nil
-//	}
 func TestResolver(t *testing.T) {
 	l, err := net.Listen("tcp", "127.0.0.1:0")
 	require.NoError(t, err)
@@ -122,14 +33,12 @@ func TestResolver(t *testing.T) {
 	serverCreds := credentials.NewTLS(tlsConfig)
 
 	srv, err := server.NewGRPCServer(&server.Config{
-		GetServerer: &getServers{}, //<label id="get_servers_mock"/>
+		GetServerer: &getServers{},
 	}, grpc.Creds(serverCreds))
 	require.NoError(t, err)
 
 	go srv.Serve(l)
-	// END: setup_test
 
-	// START: mid_test
 	conn := &clientConn{}
 	tlsConfig, err = config.SetupTLSConfig(config.TLSConfig{
 		CertFile:      config.RootClientCertFile,
@@ -144,17 +53,20 @@ func TestResolver(t *testing.T) {
 		DialCreds: clientCreds,
 	}
 	r := &loadbalance.Resolver{}
+	tAddr := "passthrough:///" + l.Addr().String()
+	targetURL, err := url.Parse(tAddr)
+	require.NoError(t, err)
+	fmt.Printf("Parsed URL: %#v\n", targetURL)
 	_, err = r.Build(
 		resolver.Target{
-			Endpoint: l.Addr().String(),
+			// resolver.Endpoint: l.Addr().String(),
+			URL: *targetURL,
 		},
 		conn,
 		opts,
 	)
 	require.NoError(t, err)
-	// END: mid_test
 
-	// START: finish_test
 	wantState := resolver.State{
 		Addresses: []resolver.Address{{
 			Addr:       "localhost:9001",
@@ -167,13 +79,11 @@ func TestResolver(t *testing.T) {
 	require.Equal(t, wantState, conn.state)
 
 	conn.state.Addresses = nil
+
 	r.ResolveNow(resolver.ResolveNowOptions{})
 	require.Equal(t, wantState, conn.state)
 }
 
-// END: finish_test
-
-// START: mock_deps
 type getServers struct{}
 
 func (s *getServers) GetServers() ([]*api.Server, error) {
@@ -192,8 +102,9 @@ type clientConn struct {
 	state resolver.State
 }
 
-func (c *clientConn) UpdateState(state resolver.State) {
+func (c *clientConn) UpdateState(state resolver.State) error {
 	c.state = state
+	return nil
 }
 
 func (c *clientConn) ReportError(err error) {}
@@ -202,8 +113,6 @@ func (c *clientConn) NewAddress(addrs []resolver.Address) {}
 
 func (c *clientConn) NewServiceConfig(config string) {}
 
-func (c *clientConn) ParseServiceConfig(
-	config string,
-) *serviceconfig.ParseResult {
+func (c *clientConn) ParseServiceConfig(config string) *serviceconfig.ParseResult {
 	return nil
 }
